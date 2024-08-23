eac655e2630036534ea28db6efd836d91c56c309
Pierre Libran <pedro89@hotmail.fr>
2024-08-23 15:23:19 +0200

    profil front done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   resources :subcategories, only: [:index, :show] do
     resources :questions, only: [:show]
   end
-
 
   resources :trainings, only: [:index, :show] do
     get 'play' => 'trainings#play'
@@ -19,7 +17,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'users/profil', to: 'users#profil'
+  resources :users, only: [] do
+    collection do
+      get :profil
+    end
+  end
+
+  get 'users/rank', to: "users#rank"
+  # get 'users/profil', to: 'users#profil'
 
 
   resources :inventories, only: [:edit, :update, :delete] do
@@ -31,7 +36,6 @@ Rails.application.routes.draw do
   # resources :users
 
   # creation de routes pour USERS/RANK
-  get 'users/rank', to: "users#rank"
   # get 'rank', to: "users#rank"
 
   # creation de routes pour USERS/show
