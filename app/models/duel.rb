9af883452b618f2d2684bb5f1350c4413d9ff49b
Pierre Libran <pedro89@hotmail.fr>
2024-08-27 19:04:55 +0200

    duel back

--- app/models/duel.rb
+++ app/models/duel.rb
@@ -3,22 +3,44 @@ class Duel < ApplicationRecord
   belongs_to :opponent, foreign_key: 'opponent_id', class_name: 'User'
 
   after_create :create_round
+  after_create :create_round_question_answers
 
   private
 
   def create_round
     2.times do
-      @subcategory = User.find(user_id).trainings.sample.subcategory
+      subcategory = User.find(user_id).trainings.sample.subcategory
       Round.create(
-          duel: self,
-          subcategory: @subcategory
+        duel: self,
+        subcategory: subcategory
       )
     end
-
-    @subcategory_opponent = User.find(opponent_id).trainings.sample.subcategory
+    subcategory_opponent = User.find(opponent_id).trainings.sample.subcategory
     Round.create(
       duel: self,
-      subcategory: @subcategory_opponent
+      subcategory: subcategory_opponent
     )
+  end
+
+  def create_round_question_answers
+    Round.where(duel: self).each do |round|
+      3.times do
+        question = round.subcategory.questions.sample
+        RoundQuestionAnswer.create(
+          user: User.find(user_id),
+          question: question,
+          round: round,
+          solved: false,
+          timer: 0
+        )
+        RoundQuestionAnswer.create(
+          user: User.find(opponent_id),
+          question: question,
+          round: round,
+          solved: false,
+          timer: 0
+        )
+      end
+    end
   end
 end
