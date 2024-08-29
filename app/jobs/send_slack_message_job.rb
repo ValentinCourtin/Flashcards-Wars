812873aeed747b035773c151eb5bc6bed016512e
Chloe Bourgeois <chloebourgeois69@gmail.com>
2024-08-29 16:53:49 +0200

    rank YOU (et 9 ou 8+you) + shop pas de choix de target si group item

--- app/jobs/send_slack_message_job.rb
+++ app/jobs/send_slack_message_job.rb
@@ -2,50 +2,32 @@ class SendSlackMessageJob < ApplicationJob
   queue_as :default
 
   def perform(target, item, user)
-    # Do something later
-
     # le slack mis sur heroku et env. => flashcardS
     notifier = Slack::Notifier.new ENV["SLACK_URL"], username: "Game Master"
 
-    # le slack mis ici a ne pas mettre sur github => flashcard
-
-
     @username = " #{user.first_name.capitalize} #{user.last_name[0].upcase}."
-    @target_username = " #{target.first_name.capitalize} #{target.last_name[0].upcase}."
 
 
-    # notifier.ping "🎮 New item used ! 🎮
-    # [#{@username}] used '#{item.name}' on [#{@target_username}] .
-    # ➡️  #{item.description}"
-
-    if item.category === "single"
+    if target.nil? || item.category == "group"
+      notifier.ping "🎮 New item used ! 🎮
+      [#{@username}] used '#{item.name}' on [everyone].
+      ➡️  #{item.description} "
+    elsif target.present? || item.category == "single"
+      @target_username = " #{target.first_name.capitalize} #{target.last_name[0].upcase}."
       notifier.ping "🎮 New item used ! 🎮
     [#{@username}] used '#{item.name}' on [#{@target_username}] .
     ➡️  #{item.description} "
-    elsif item.category === "group"
-      notifier.ping "🎮 New item used ! 🎮
-    [#{@username}] used '#{item.name}' on [everyone].
-    ➡️  #{item.description} "
     end
+
+    # if item.category === "single"
+    #   notifier.ping "🎮 New item used ! 🎮
+    # [#{@username}] used '#{item.name}' on [#{@target_username}] .
+    # ➡️  #{item.description} "
+    # elsif item.category === "group"
+    #   notifier.ping "🎮 New item used ! 🎮
+    # [#{@username}] used '#{item.name}' on [everyone].
+    # ➡️  #{item.description} "
+    # end
 
   end
 end
-
-
-# notifier.ping "#{user.first_name} #{target.first_name} #{item.name} #{item.description}"
-
-
-# notifier.ping "🎮 New item used ! 🎮
-# [#{user.first_name}] used '#{item.name}' on [#{target.first_name}] .
-# ➡️  #{item.description} #{@username}"
-
-
-# notifier.ping "🎮 New item used ! 🎮
-# [#{@username}] used '#{item.name}' on [#{@target_username}] .
-# ➡️  #{item.description} "
-#   end
-# end
-
-
-# "https://hooks.slack.com/services/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"
-# "https://hooks.slack.com/services/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"
